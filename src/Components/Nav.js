f0b2c6cb83ff80aa2cb8455c2453808b79805f69
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2020-09-11 11:47:50 -0500

    navBar [x]

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,19 +1,45 @@
 import React from 'react';
 import Logo from '../Assets/shamazon-logo.png';
 import './../Styles/Nav.scss';
-
+import {Link} from 'react-router-dom'
+import SearchIcon from '@material-ui/icons/Search';
+import BasketIcon from '@material-ui/icons/ShoppingBasket'
 function Nav() {
     return (
         <nav className="navBar">
-        {/*  logo on left */}
         <img className="navBar__logo" 
         src={Logo} 
         alt=""/>
-        {/* search box */}
-        
-        {/* <input></input> */}
-        {/*  3 links */}
-        {/* basket with number */}
+        <div className="navBar__searchContainer">
+        <input type="text" className="navBar__input"/>
+        <SearchIcon className="navBar__searchIcon"/>
+        </div>
+        <div className="navBar__nav">
+            <Link to="/login" className="navBar__link">
+            <div className="navBar__option">
+            <span className='nav__optionLineOne'>Hello,</span>
+            <span className='nav__optionLineTwo'>Sign in</span>
+            </div>
+            </Link>
+            <Link to="/login" className="navBar__link">
+            <div className="navBar__option">
+            <span className='nav__optionLineOne'>Returns</span>
+            <span className='nav__optionLineTwo'>& Orders</span>
+            </div>
+            </Link>
+            <Link to="/login" className="navBar__link">
+            <div className="navBar__option">
+            <span className='nav__optionLineOne'>Your</span>
+            <span className='nav__optionLineTwo'>Prime</span>
+            </div>
+            </Link>
+            <Link to="/checkout" className="navBar__link">
+                <div className="nav__basket">
+                <BasketIcon/>
+                <span className="nav_optionLineTwo nav__basketCount">0</span>
+                </div>
+            </Link>
+        </div>
         </nav>
     )
 }
